5c1e6eb987f4c555044a1cdf8e7f9ab085d23128
unknown <khoinom2018.gmail.com>
2022-08-16 13:28:48 +0100

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import SearchBox from './SearchBox';
 import Cardlist from './Cardlist';
-import users from './users.json';
+// import users from './users.json';
 import "./App.css";
 import React from 'react';
 
@@ -9,7 +9,9 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      searchInput: ''
+      searchInput: '',
+      kittens:[],
+      loading: false
     }
   }
 
@@ -18,21 +20,30 @@ class App extends React.Component {
     this.setState({ searchInput: event.target.value })
   }
 
+  componentDidMount(){
+    this.setState({loading:true})
 
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => this.setState({kittens:json,loading:false}))
+  .catch((error)=>console.log(error))
+  }
 
   render() {
 
-    const filteredRobots = users.filter(item => (
+    const filteredKittens = this.state.kittens.filter(item => (
       item.name.toLowerCase().includes(this.state.searchInput.toLowerCase())
     ))
 
-    console.log("Filtered Robots: ", filteredRobots);
+    // console.log("Filtered Kittens: ", filteredKittens);
 
     return (
       <div className="back">
         <h1 style={{ textAlign: 'center'}}>[G-kittens]</h1>
         <SearchBox updateSearchInput={this.updateSearchInput}/>
-        <Cardlist clients={filteredRobots}/>
+        <Cardlist clients={filteredKittens}/>
+        {this.state.loading && <h1 style={{textAlign:'center'}}>Loadiing...</h1>}
+        {!filteredKittens.length && !this.state.loading && <h3 style={{textAlign:'center'}}>No Kittens available!</h3>}
       </div>
     );     
   }
